68dc8a4e217758a8625ac478c01c8bfcc196f0a3
Alisson G Costa <alisson.costa84200@gmail.com>
2026-07-09 21:46:29 -0300

    feat(header): update Header component styling and layout

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { MdOutlineOpenInNew } from "react-icons/md";
+import { MdMenu, MdOutlineOpenInNew } from "react-icons/md";
 
 
 export const Header = () => {
@@ -9,22 +9,27 @@ export const Header = () => {
 
     return(
         <header>
-            <nav className= "flex items-center justify-center bg-primary py-2" >
+            <nav className= "flex items-center gap-6 justify-start md:justify-center bg-primary py-2 px-6" >
+                <button className= "sm:hidden">
+                    <MdMenu size = {24}/>
+                </button>
+
                 <ul className= "flex gap-4 items-center">
                     <li className= "my-2">
                         <Link href="/" className= "border-2 rounded-md py-1 px-2 fount-bold">
                             CODARSE
                         </Link>
                     </li>
-                    <li>
+
+                    <li className= "hidden sm:block">
                         <Link href="/" data-active ={currentPath === "/"} className="data-[active=true]:underline" >
                             Pagina inicial
                         </Link>
                     </li>
-                    <li>
+                    <li className= "hidden sm:block">
                         <Link href="/cursos" data-active ={currentPath === "/cursos"} className="data-[active=true]:underline">Cursos</Link>
                     </li>
-                    <li >
+                    <li className= "hidden sm:block" >
                         <Link className= "flex items-center gap-1" href="https://blog.cordase.com" target = "_blank" >
                         blog
                         <MdOutlineOpenInNew/>
@@ -32,6 +37,9 @@ export const Header = () => {
                     </li>
                     
                 </ul>
+                <h1 className="sm:hidden">
+                    Codarse - pagina inicial
+                </h1>
             </nav>
 
         </header>
