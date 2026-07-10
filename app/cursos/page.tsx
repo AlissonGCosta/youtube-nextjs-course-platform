e6e50220997f04667c01b610f3e007d7c28dc210
Alisson Costa <alisson@example.com>
2026-07-09 23:02:55 -0300

    feat: add metadata for home and courses pages

--- app/cursos/page.tsx
+++ app/cursos/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata:Metadata = {
+  title: "CodarSe - todos os cursos",
+};
+
+
 export default function PageCursos(){
     return (
         <main>
