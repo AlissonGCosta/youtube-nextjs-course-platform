a52191b09d2dc8e8803c2a282eed427d2604135e
Alisson G Costa <alisson.costa84200@gmail.com>
2026-07-09 23:09:18 -0300

    Revert "feat: add metadata for home and courses pages"
    This reverts commit a1aa04e47ce6d27c2f44d5a1ebef904e4a116d87.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
 import Image from "next/image";
-import type { Metadata } from "next";
-
-export const metadata:Metadata = {
-  title: "CodarSe - pagina inicial",
-};
 
 export default function PageHome() {
   return (
