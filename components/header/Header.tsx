dcaee4aca309c78f9a1f8f0cdea993b40cecc0a4
costa <costa@example.com>
2026-07-13 23:39:12 -0300

    fix(header): update Header component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,77 +1,145 @@
-'use client';
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { MdMenu, MdOutlineOpenInNew } from "react-icons/md";
 
-
 export const Header = () => {
-    const [drawer, setDrawer] =  useState(false);
-    const [title, setTitle] =  useState('CodarSe');
-    const currentPath = usePathname();
+  const [drawer, setDrawer] = useState(false);
+  const [title, setTitle] = useState("CodarSe");
+  const currentPath = usePathname();
 
-    useEffect(() => {
-            // eslint-disable-next-line react-hooks/set-state-in-effect
-            setDrawer(false);
-            setTitle(document.title);
-    }, [currentPath]);
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setDrawer(false);
+    setTitle(document.title);
+  }, [currentPath]);
 
-    return(
-        <header>
-            <nav className= "flex items-center gap-6 justify-start md:justify-center bg-primary py-2 px-6" >
+  return (
+    <header>
+      <nav
+        className="
+          flex
+          py-2 px-6
+          bg-primary
+          items-center gap-6 justify-start
+          md:justify-center
+        "
+      >
+        <button
+          onClick={() => setDrawer(true)}
+          className="
+            sm:hidden
+          "
+        >
+          <MdMenu size={24} />
+        </button>
 
-                <button className= "sm:hidden" onClick={() => setDrawer(true)}>
-                    <MdMenu size = {24}/>
-                </button>
+        <ul
+          tabIndex={drawer ? -1 : undefined}
+          className="
+            flex
+            gap-4 items-center
+          "
+        >
+          <li
+            className="
 
-                <ul className= "flex gap-4 items-center" tabIndex={drawer ? -1 : undefined}>
-                     <li className= "">
-                        <Link href="/" className= "border-2 rounded-md py-1 px-2 fount-bold">
-                            CODARSE
-                        </Link>
-                    </li>
-                </ul>
+            "
+          >
+            <Link
+              href="/"
+              className="
+                py-1 px-2
+                border-2 rounded-md
+                  font-bold
+              "
+            >
+              CODARSE
+            </Link>
+          </li>
+        </ul>
 
-                <div data-open={drawer}
-                 tabIndex={drawer ? undefined : -1}
-                 onClick={() => setDrawer(false)}
-                 className= "bg-gradient-to-r from-background fixed top-0 left-0 bottom-0 right-0 transition-transform data-[open=false]:-translate-x-full">
+        <div
+          data-open={drawer}
+          tabIndex={drawer ? undefined : -1}
+          onClick={() => setDrawer(false)}
+          className="
+            bg-gradient-to-r from-background
+            transition-transform
+            fixed top-0 left-0 bottom-0 right-0 data-[open=false]:-translate-x-full
+          "
+        >
+          <ul
+            onClick={(e) => e.stopPropagation()}
+            className="
+              flex flex-col
+              w-60 h-full
+              p-4
+              bg-background
+              gap-4
+            "
+          >
+            <li
+              className="
 
-                     <ul onClick={e => e.stopPropagation()}
-                     className= "flex gap-4 flex-col  p-4 w-60 bg-background h-full">
-                   
+              "
+            >
+              <Link
+                href="/"
+                data-active={currentPath === "/"}
+                className="
+                  data-[active=true]:underline
+                "
+              >
+                Pagina inicial
+              </Link>
+            </li>
 
-                        <li className= "">
-                            <Link href="/" data-active ={currentPath === "/"} className="data-[active=true]:underline" >
-                                Pagina inicial
-                            </Link>
-                        </li>
+            <li
+              className="
 
-                        <li className= "">
-                            <Link href="/cursos" data-active ={currentPath === "/cursos"} className="data-[active=true]:underline">
-                                Cursos
-                            </Link>
-                        </li>
+              "
+            >
+              <Link
+                href="/cursos/"
+                data-active={currentPath === "/cursos"}
+                className="
+                  data-[active=true]:underline
+                "
+              >
+                Cursos
+              </Link>
+            </li>
 
-                        <li className= "" >
-                            <Link className= "flex items-center gap-1" href="https://blog.cordase.com" target = "_blank" >
-                            blog
-                            <MdOutlineOpenInNew/>
-                            </Link>
-                        </li>
-                    
-                    </ul>
+            <li
+              className="
 
-                </div>
+              "
+            >
+              <Link
+                href="https://blog.cordase.com"
+                target="_blank"
+                className="
+                  flex
+                  items-center gap-1
+                "
+              >
+                blog
+                <MdOutlineOpenInNew />
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-               
-                <h1 className="sm:hidden">
-                 {title}
-                </h1>
-            </nav>
-
-        </header>
-    );
+        <h1
+          className="
+            sm:hidden
+          "
+        >
+          {title}
+        </h1>
+      </nav>
+    </header>
+  );
 };
-
-
